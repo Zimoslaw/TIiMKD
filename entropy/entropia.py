389ed019334e605ed2afed8388c5695aed269513
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2023-11-22 00:32:07 +0100

    Started implementing entropy of higher levels

--- entropy/entropia.py
+++ entropy/entropia.py
@@ -1,56 +1,83 @@
-import sys, getopt, math
+import sys
+import getopt
+import math
+
 
 def main(argv):
     file = ''
     entropy_type = ''
     level = 0
-    opts, args = getopt.getopt(argv,"hf:t:n")
+    opts, args = getopt.getopt(argv, "hf:t:n:")
     for opt, arg in opts:
         if opt == '-h':
-            print ('entropia.py -f <filename> -t <C|W> -n [number]')
-            print ('filename - file with text to be analyzed. Text should be in english alphabet.')
-            print ('C - characters entropy. W - words entropy')
-            print ('number - number of characters/words used in conditional entropy')
+            print('Usage:')
+            print('entropia.py -f <filename> -t <C|W> -n [number]')
+            print('Parameters:')
+            print('filename - file with text to be analyzed. Text should be in english alphabet.')
+            print('C - characters entropy. W - words entropy')
+            print('number - number of characters/words used in conditional entropy')
+            print('Parameter -n is optional.')
             sys.exit()
         elif opt == '-f':
             file = arg
         elif opt == '-t':
             entropy_type = arg
-            if entropy_type != 'C' and entropy_type !='W':
+            if entropy_type != 'C' and entropy_type != 'W':
                 print("Invalid it argument. Expected 'C' or 'W'")
                 sys.exit()
         elif opt == '-n':
-            level = arg
+            level = int(arg)
 
     content = open(file, 'r')
-    
+
     entropy(content.read(), entropy_type, level)
+
 
 def entropy(text, type, level):
     # Tworzenie alfabetu
     alphabet = {}
-    if type == 'W': # Entropia słów
-        text = text.split()
+    alphabet_secondary = {}
+    if type == 'W':  # Entropia słów
+        text = text.split() # Alfabet zamiast z liter składać się będzie ze słów
 
-    for c in text:
-        if c in alphabet.keys():
-            alphabet[c] += 1
-            continue
-        alphabet[c] = 1
+    unique_elements = list(text)
+    for c in range(0, len(unique_elements)):
+        char = unique_elements[c]
+        if c + level < len(unique_elements):
+
+            if level > 0: # Pomocniczy alfabet
+                if char in alphabet_secondary.keys():
+                    alphabet_secondary[char] += 1
+                else:
+                    alphabet_secondary[char] = 1
+
+            for i in range(1, level + 1):
+                char += unique_elements[c+i]
+
+            if char in alphabet.keys():
+                alphabet[char] += 1
+                continue
+            alphabet[char] = 1
 
     print(str(alphabet))
 
-    # Prawdopodobieństwa
+    # Prawdopodobieństwo elementu / Prawdopodobieństwo wspólne
+    probabilities = {}
     for k in alphabet.keys():
-        alphabet[k] = alphabet[k]/len(text)
+        probabilities[k] = alphabet[k] / len(text)
+
+    # Prawdopodobieństwo warunkowe
+    #if level > 0:
+
 
     # Obliczanie entropii
     sum = 0
-    for k in alphabet.keys():
-        sum += alphabet[k] * math.log(alphabet[k], 2)
+    for k in probabilities.keys():
+        sum += probabilities[k] * math.log(probabilities[k], 2)
     sum *= -1
 
     print('Entropy =', sum)
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
